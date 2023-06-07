72cd5f32f96a838357ac5b78818883946ba9388d
Oleg <dobrunja90@yahoo.ca>
2023-06-07 00:14:33 -0600

    Added web token system for hashing passwords. added defs and resolvers for it

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -1,5 +1,6 @@
 const { User, Thought } = require('../models');
 const { AuthenticationError } = require('apollo-server-express');
+const { signToken } = require("../utils/auth");
 const resolvers = {
     Query: {
       thoughts: async (parent, { username }) => {
@@ -22,14 +23,27 @@ const resolvers = {
           .select('-__v -password')
           .populate('friends')
           .populate('thoughts');
+      },
+      me: async (parent, args, context) => {
+        if (context.user) {
+          const userData = await User.findOne({ _id: context.user._id })
+            .select('-__v -password')
+            .populate('thoughts')
+            .populate('friends');
+      
+          return userData;
+        }
+      
+        throw new AuthenticationError('Not logged in');
       }
     },  
 
     Mutation: {
       addUser: async (parent, args) => {
         const user = await User.create(args);
+        const token = signToken(user);
         
-        return user;
+        return {token,user};
       },
       login: async (parent, { email, password }) => {
         const user = await User.findOne({ email });
@@ -43,8 +57,9 @@ const resolvers = {
         if (!correctPw) {
           throw new AuthenticationError('Incorrect credentials');
         }
-      
-        return user;
+        
+        const token = signToken(user);
+        return {user, token};
       }
       
     }
